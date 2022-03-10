5322bd6af725ebf7763f1bf517081eb3636071e4
tuanmmm <nguyenthetuan100902@gmail.com>
2022-03-10 13:50:36 +0700

    buoi2

---
+++ my-vue-app/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import ShowInfo from './components/Showinfo'
+function App() {
+  const [count,setCount] = useState(0);
+  const [status,setStatus] = useState(false)
+  const [products, setProducts] = useState([
+         {id: 1, name: "Product A"},
+         {id: 2, name: "Product B"},
+         {id: 3, name: "Product C"},
+    ]); 
+  return (   
+    <div>
+      Count : {count} <button onClick={() => setCount(count +1)}>click</button>
+      <button onClick={() => setCount(count -1)}>click giảm</button>
+      {products.map((item,index) => <div key={index}>{item.name}</div>)}
+        <ShowInfo name="tuan" />
+    </div>
+  )
+}
+
+export default App
